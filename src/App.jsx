81a0dbbc28a5c78bfac27f612d7cb76b6d9c69e9
SBEGENJOV <seydi19981995@gmail.com>
2024-02-23 21:31:26 +0300

    update/router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Pages/HomePage";
 import ShopPage from "./Pages/ShopPage";
@@ -35,42 +35,47 @@ import UpdateSliderPage from "./Pages/admin/Slider/UpdateSliderPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/auth" element={<AuthPage />} />
-      <Route path="/shop" element={<ShopPage />} />
-      <Route path="/user" element={<UserPage />} />
-      <Route path="/contact" element={<ContactPage />} />
-      <Route path="/cart" element={<CartPage />} />
-      <Route path="/product/:id" element={<ProductDetailsPage />} />
-      <Route path="/productCategory/:id" element={<ProductCategory />} />
-      <Route path="/blog/:id" element={<BlogDetailsPage />} />
-      <Route path="/blog" element={<Blogs />} />
-      <Route path="/success" element={<Success />} />
-      <Route path="/alfa/*">
-        <Route path="users" element={<AdminUserPage />} />
-        <Route path="categories" element={<CategoryPage />} />
-        <Route path="categories/create" element={<CreateCategoryPage />} />
-        <Route path="categories/update/:id" element={<UpdateCategoryPage />} />
-        <Route path="products/create" element={<CreateProductPage />} />
-        <Route path="products" element={<ProductPage />} />
-        <Route path="products/update/:id" element={<UpdateProductPage />} />
-        <Route path="coupons" element={<CouponPage />} />
-        <Route path="coupons/create" element={<CreateCouponPage />} />
-        <Route path="coupons/update/:id" element={<UpdateCouponPage />} />
-        <Route path="orders" element={<OrderPage />} />
-        <Route path="logo" element={<LogoPage />} />
-        <Route path="logo/update/:id" element={<UpdateLogoPage />} />
-        <Route path="slider" element={<SliderPage />} />
-        <Route path="slider/create" element={<CreateSliderPage />} />
-        <Route path="slider/update/:id" element={<UpdateSliderPage />} />
-        <Route path="blog" element={<BlogPage />} />
-        <Route path="blog/create" element={<CreateBlogPage />} />
-        <Route path="blog/update/:id" element={<UpdateBlogPage />} />
-        <Route path="contact" element={<ContactAdmin />} />
-        <Route index element={<DashboardPage />} />
-      </Route>
-    </Routes>
+    <Router>
+      <Switch>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/user" element={<UserPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/product/:id" element={<ProductDetailsPage />} />
+        <Route path="/productCategory/:id" element={<ProductCategory />} />
+        <Route path="/blog/:id" element={<BlogDetailsPage />} />
+        <Route path="/blog" element={<Blogs />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/alfa/*">
+          <Route path="users" element={<AdminUserPage />} />
+          <Route path="categories" element={<CategoryPage />} />
+          <Route path="categories/create" element={<CreateCategoryPage />} />
+          <Route
+            path="categories/update/:id"
+            element={<UpdateCategoryPage />}
+          />
+          <Route path="products/create" element={<CreateProductPage />} />
+          <Route path="products" element={<ProductPage />} />
+          <Route path="products/update/:id" element={<UpdateProductPage />} />
+          <Route path="coupons" element={<CouponPage />} />
+          <Route path="coupons/create" element={<CreateCouponPage />} />
+          <Route path="coupons/update/:id" element={<UpdateCouponPage />} />
+          <Route path="orders" element={<OrderPage />} />
+          <Route path="logo" element={<LogoPage />} />
+          <Route path="logo/update/:id" element={<UpdateLogoPage />} />
+          <Route path="slider" element={<SliderPage />} />
+          <Route path="slider/create" element={<CreateSliderPage />} />
+          <Route path="slider/update/:id" element={<UpdateSliderPage />} />
+          <Route path="blog" element={<BlogPage />} />
+          <Route path="blog/create" element={<CreateBlogPage />} />
+          <Route path="blog/update/:id" element={<UpdateBlogPage />} />
+          <Route path="contact" element={<ContactAdmin />} />
+          <Route index element={<DashboardPage />} />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
